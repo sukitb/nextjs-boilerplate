83e2c673dfe2d1541786fb08b5cfadd44e857cab
sukit-grow-it <155421330+sukit-grow-it@users.noreply.github.com>
2024-06-19 13:14:34 +0700

    Set unit testing with vitest

---
+++ src/app/page.test.tsx
@@ -0,0 +1,9 @@
+import { render, screen } from "@testing-library/react";
+import Page from "./page";
+
+describe("Page", () => {
+  it("renders the page", () => {
+    render(<Page />);
+    expect(screen.getByText("Get started by editing")).toBeInTheDocument();
+  });
+});
