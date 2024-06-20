fca8f9f0bea172a02545d0eb5f2d4e098ab0457a
sukit-grow-it <155421330+sukit-grow-it@users.noreply.github.com>
2024-06-20 21:43:16 +0700

    Set automation to aws ecs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
 import Image from "next/image";
 
+import { getBaseUrl } from "@/utils/Helpers";
+
+const baseUrl = getBaseUrl();
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1>This is GrowsIT NextJS Boilerplate</h1>
+      {baseUrl}
     </main>
   );
 }
